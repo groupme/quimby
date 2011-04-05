fc9ef6d25bd60b50a9ff272c2215e278770497fd
Pierre Valade <pierre@valade.info>
2011-04-05 20:10:31 +0200

    added methods (authorize_url, access_token to Foursquare::Base to oauth easily

--- lib/foursquare/base.rb
+++ lib/foursquare/base.rb
@@ -48,8 +48,55 @@ module Foursquare
       Foursquare.log(response.inspect)
       error(response) || response["response"]
     end
+    
+    def authorize_url(redirect_uri)
+      # http://developer.foursquare.com/docs/oauth.html
+      
+      # check params
+      raise "you need to define a client id before" if @client_id.blank?
+      raise "no callback url provided" if redirect_uri.blank?
+      
+      # params
+      params = {}
+      params["client_id"] = @client_id
+      params["response_type"] = "code"
+      params["redirect_uri"] = redirect_uri
+      
+      # url
+      oauth2_url('authenticate', params)
+    end
+    
+    def access_token(code, redirect_uri)
+      # http://developer.foursquare.com/docs/oauth.html
+      
+      # check params
+      raise "you need to define a client id before" if @client_id.blank?
+      raise "you need to define a client secret before" if @client_secret.blank?
+      raise "no code provided" if code.blank?
+      raise "no redirect_uri provided" if redirect_uri.blank?
+      
+      # params
+      params = {}
+      params["client_id"] = @client_id
+      params["client_secret"] = @client_secret
+      params["grant_type"] = "authorization_code"
+      params["redirect_uri"] = redirect_uri
+      params["code"] = code
+      
+      # url
+      url = oauth2_url('access_token', params)
+      
+      # response
+      # http://developer.foursquare.com/docs/oauth.html
+      response = JSON.parse(Typhoeus::Request.get(url).body)
+      response["access_token"]
+    end
 
     private
+    
+    def oauth2_url(method_name, params)
+      "https://foursquare.com/oauth2/#{method_name}?#{params.to_query}"
+    end
 
     def camelize(params)
       params.inject({}) { |o, (k, v)|
