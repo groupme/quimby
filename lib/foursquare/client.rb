cc5124c3ff0dcae34cce0e353ed12489b4df7063
Cory Flanigan <seeflanigan@gmail.com>
2011-07-11 18:25:57 -0600

    Split initialization and configuration into the API class and make Client inherit from it

--- lib/foursquare/client.rb
+++ lib/foursquare/client.rb
@@ -1,12 +1,5 @@
 module Foursquare
-  class Client
-    def initialize(options = {})
-      @api = options.delete(:url) || "https://api.foursquare.com/v2/"
-      @auth = options.delete(:auth)
-      unless @auth
-        raise ArgumentError, "A username and password, access_token, or client_id and client_secret are required"
-      end
-    end
+  class Client < API
 
     private
     def valid_date_string
