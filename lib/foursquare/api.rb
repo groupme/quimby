e7a1d1189712e568b74ca92423ffdbcd4072b9ba
Cory Flanigan <seeflanigan@gmail.com>
2011-07-13 09:09:38 -0600

    Add API class to start abstracting away API interaction

--- lib/foursquare/api.rb
+++ lib/foursquare/api.rb
@@ -1,11 +1,4 @@
 module Foursquare
   class API < Base
-    def initialize(options = {})
-      @api = options.delete(:url) || "https://api.foursquare.com/v2/"
-      @auth = options.delete(:auth)
-      unless @auth
-        raise ArgumentError, "A username and password, access_token, or client_id and client_secret are required"
-      end
-    end
   end
 end
