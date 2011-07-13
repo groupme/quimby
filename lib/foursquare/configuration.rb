398ca0f98a4c14c56217bb2c06ed75afa75897f2
Cory Flanigan <seeflanigan@gmail.com>
2011-07-13 08:29:58 -0600

    Add specs for Configuration, set up default options, read config options in from a file

--- lib/foursquare/configuration.rb
+++ lib/foursquare/configuration.rb
@@ -2,7 +2,10 @@ require "foursquare/version"
 
 module Foursquare
   class Configuration
-    DEFAULTS = {}
+    # Load the configuration file
+    CONFIG_FILE = YAML.load_file('config.yml')
+
+    # Define keys that will be valid for passing in options
     VALID_OPTION_KEYS = [:endpoint,
                          :http_username,
                          :http_password,
@@ -10,18 +13,42 @@ module Foursquare
                          :oauth_consumer_key,
                          :oauth_consumer_secret].freeze
 
-    # Most options default to nil
+    # Generate accessors for configuration options
+    VALID_OPTION_KEYS.each {|k| attr_accessor k}
+
+    # Define a container for defaults
+    DEFAULTS = {}
+
+    # Initialize defaults to nil
     VALID_OPTION_KEYS.each {|key| DEFAULTS[key] = nil}
 
     # Override nil options here
     DEFAULTS[:endpoint] = "https://api.foursquare.com/v2/"
+
+    # Prevent further modifications to defaults
     DEFAULTS.freeze
 
-    VALID_OPTION_KEYS.each {|k| attr_accessor k}
-
     def initialize(options={})
+      # Read in options from config file
+      env_sym = Foursquare.env.to_sym
+      config_file_hash = CONFIG_FILE[env_sym]
+      config_file_opts = {}
+      
+      # HACK because config file layout and
+      # config object structure are not identical
+      config_file_hash[:api].each do |key, val|
+        config_file_opts[key] = val
+      end
+      config_file_hash[:auth].each do |key, val|
+        config_file_opts[key] = val
+      end
+
+      # Initialize values 
+      # (is there a way to do this en masse, instead of iterating?)
       VALID_OPTION_KEYS.each do |key|
-        option = options.delete(key) || DEFAULTS[key]
+        option = options.delete(key) || 
+          config_file_opts[key] || 
+          DEFAULTS[key]
         self.send("#{key}=", option)
       end
     end
