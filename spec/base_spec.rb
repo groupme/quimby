6450273c81f96d40f709fed06c8f9f9b8000c117
Cory Flanigan <seeflanigan@gmail.com>
2011-07-13 08:32:56 -0600

    Update spec to accomodate current config layout

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 describe "base" do
   it "defines url on the returned json object for a venue" do
     VCR.use_cassette('denver_zoo') do
-      @four = Foursquare::Base.new(APPCONFIG[:user][:client_id], APPCONFIG[:user][:client_secret])
+      oauth_key = APPCONFIG[:auth][:oauth_consumer_key]
+      oauth_secret = APPCONFIG[:auth][:oauth_consumer_secret]
+      @four = Foursquare::Base.new(oauth_key, oauth_secret)
       @denver_zoo = @four.venues.find('118057')
       @denver_zoo.url.should == 'http://www.denverzoo.org'
     end
